4043baef12fd67689ca426b438968d93b316a8bb
Taylor Wu <Taylorwu21@gmail.com>
2016-05-11 12:07:11 -0600

    pseudocode for basic calculator

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,16 @@
+# create / edit a file
+# stage the file - git add
+# make a commit
+# push to a github repository
 puts "calculator"
+puts "Enter your first number:"
+# get the first number
+puts "enter your operator"
+# get the operator
+puts "enter the second number:"
+# get the second number
+
+# execute the equation
+# output results
+
+# do all the bonus objectives
