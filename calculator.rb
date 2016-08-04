dae01cae15dcbf972e9b8bfd8515183acfec04b2
Taylor Wu <Taylorwu21@gmail.com>
2016-08-03 18:59:14 -0600

    added history

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,10 @@
+require 'pry'
 # create / edit a file
 # stage the file - git add
 # make a commit
 # push to a github repository
+@history = []
+
 def first
   puts "Enter the first number:"
   @first_number = gets.strip.to_f
@@ -26,34 +29,64 @@ def operator
   @user_operation = gets.strip.to_i
 end
 
+# def error_checking(/^[-+]?\d*\.?\d+$/)
+#   @first_number
+# end
+
 def calculate
   print "The Answer is: "
   case @user_operation
   when 1
-    puts @first_number + @second_number
+    @answer = @first_number + @second_number
+    puts "The answer is: #{@answer}"
   when 2
-    puts @first_number - @second_number
+    @answer = @first_number - @second_number
+    puts "The answer is: #{@answer}"
   when 3
-    puts @first_number * @second_number
+    @answer = @first_number * @second_number
+    puts "The answer is: #{@answer}"
   when 4
     if @second_number == 0
       puts "Cannot divide by 0"
     else 
-      puts @first_number / @second_number
+      @answer = @first_number / @second_number
+      puts "The answer is: #{@answer}"
     end
   when 5
-    puts @first_number ** @second_number
+    @answer = @first_number ** @second_number
+    puts "The answer is: #{@answer}"
   else
     puts "I only support +, -, *, /, ^"
   end
 end
 
-while true
+def history_option
+  @first_number = @answer
+  operator
+  second
+  calculate
+  history_call
+end
+
+def history_call
+  puts "Would you like to use the previous answer? (y/n) or quit with q"
+  user_history_input = gets.strip.downcase
+  if user_history_input == 'n'
+    play
+  elsif user_history_input == 'q'
+    exit(0)
+  else
+    history_option
+  end
+end
+
+def play
   puts "--------Calculator--------"
   first
   operator
   second
   calculate
-  puts "To quit press q or press enter to continue"
-  exit(0) if gets.strip == 'q'
+  history_call
 end
+
+play
